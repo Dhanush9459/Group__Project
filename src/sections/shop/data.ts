14625385d5dea9ba754c62b74a3826a1e53183d0
Jyothsna <jl2426@njit.edu>
2023-12-18 15:39:40 -0500

    added services section and cookie related files

---
+++ src/sections/shop/data.ts
@@ -0,0 +1,29 @@
+const data: {
+  id: number;
+  title: string;
+  description: string;
+  price: string;
+  logoBg: string;
+}[] = [
+  {
+    id: 1,
+    title: "C A T E R I N G",
+    description:
+      "At Soul Kitchen we pride ourselves on fresh flavours coupled with impeccable service.Please call us for further enquiry.",
+    price: "",
+    logoBg: "",
+  },
+
+  {
+    id: 2,
+    title: "D I N E - I N",
+    description:
+      "Join us for a dining experience that will leave you with a newfound appreciation for the rich tapestry of Indian cuisine.",
+    price: "",
+    logoBg: "",
+  },
+
+ 
+];
+
+export default data;
